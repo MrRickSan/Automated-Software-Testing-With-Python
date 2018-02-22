7ba96f927b3a9fbf5dc511da52b5960bb666d29c
DESKTOP-0D0D8FB\Ricardo Oliveira <ricardooliveira.info@gmail.com>
2018-02-21 23:02:50 -0300

    First unit test created

--- Python Refresher/dictionaries.py
+++ Python Refresher/dictionaries.py
@@ -2,16 +2,11 @@ my_set = {1, 3, 5}
 my_dict = {'name': 'Jose', 'age': 90}
 another_dict = {1: 15, 2: 75, 3: 150}
 
-lottery_players = [
-    {
+lottery_players ={
     'name': 'Rolf',
     'numbers': (13, 45, 66, 23, 22)
-    },
-    {
-    'name': 'John',
-    'numbers': (14, 56, 80, 23, 22)
     }
-]
+
 universities = [
     {
         'name': 'Oxford',
