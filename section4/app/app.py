afad5cec9b4be185319fd1899088fe8c71312466
DESKTOP-0D0D8FB\Ricardo Oliveira <ricardooliveira.info@gmail.com>
2018-02-27 23:31:31 -0300

    End of section4

--- section4/app/app.py
+++ section4/app/app.py
@@ -1,3 +1,10 @@
-from flask import  Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify({'message': 'Hello, world!'})
+
+if __name__ == '__main__':
+    app.run()
