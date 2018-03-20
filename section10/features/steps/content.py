70789a18794aa6f4c3af0bac11b7d11e12545f25
DESKTOP-0D0D8FB\Ricardo Oliveira <ricardooliveira.info@gmail.com>
2018-03-20 00:13:41 -0300

    section10 base_page

--- section10/features/steps/content.py
+++ section10/features/steps/content.py
@@ -1,16 +1,16 @@
 from behave import *
 
-from features.locators.home_page import HomePageLocators
+from features.page_model.base_page import BasePage
 
 use_step_matcher('re')
 
 
 @then('There is a title shown on the page')
 def step_impl(context):
-    title_tag = context.browser.find_element(*HomePageLocators.TITLE)
-    assert title_tag.is_displayed()
+    page = BasePage(context.driver)
+    assert page.title.is_displayed()
 
 @step('The title tag has content "(.*)"')
 def step_impl(context, content):
-    title_tag = context.browser.find_element(*HomePageLocators.TITLE)
-    assert title_tag.text == content
+    page = BasePage(context.driver)
+    assert page.title.text == content
